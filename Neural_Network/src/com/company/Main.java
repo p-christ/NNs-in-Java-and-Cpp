f782dfbaf47936a0bbc76314364d64e4dd7c5357
p-christ <p.christodoulou2@gmail.com>
2019-10-18 21:46:08 +0100

    generates training data randomly

--- Neural_Network/src/com/company/Main.java
+++ Neural_Network/src/com/company/Main.java
@@ -11,15 +11,36 @@ public class Main {
 
         ArrayList<Integer> layers = new ArrayList<>(Arrays.asList(10,10,1));
         double learningRate = 0.1;
+        int rows = 5;
+        int cols = 8;
+
+        FNN nn = new FNN(layers, learningRate);
+
+        double[][] trainingData = generateRandomTrainingData(rows, cols);
 
 
-        FNN nn = new FNN(layers, learningRate);   // , learningRate);
+
+
+
 
         // write your code here
     }
 
+
+    public static double[][] generateRandomTrainingData(int rows, int cols){
+
+        double[][] trainingData = new double[rows][cols];
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < cols ; j++){
+                trainingData[i][j] = Math.random();
+            }
+        }
+        return trainingData;
+    }
+
+
     // forward pass  ... store data
     // backward pass
     // initialisation --> random weights and creating arrays
-    
+
 }
